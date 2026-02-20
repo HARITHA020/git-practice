bbf0915769e282d1e5cb8281ac9c2f0cdc227e64
haritha <haritharavichandran11@gmail.com>
2026-02-20 20:27:17 +0530

    the new features is added

--- src/com/practice/git/HelloGit.java
+++ src/com/practice/git/HelloGit.java
@@ -5,6 +5,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("hello git ");
 		System.out.println("second commit");
+		System.out.println("the new features is commit");
 
 	}
 
